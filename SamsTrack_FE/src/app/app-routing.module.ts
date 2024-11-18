b1f7ef038202cfb8b5c380088f7928371241e595
Nihal <nihalbarhaiyya@gmail.com>
2024-11-18 08:04:39 +0530

    FE

--- SamsTrack_FE/src/app/app-routing.module.ts
+++ SamsTrack_FE/src/app/app-routing.module.ts
@@ -2,6 +2,14 @@ import { LoginComponent } from './components/login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { AddUserComponent } from './components/add-user/add-user.component';
+import { AllUserComponent } from './components/all-user/all-user.component';
+import { AllStudentComponent } from './components/all-student/all-student.component';
+import { AddStudentComponent } from './components/add-student/add-student.component';
+import { AllSubjectComponent } from './components/all-subject/all-subject.component';
+import { AddSubjectComponent } from './components/add-subject/add-subject.component';
+import { TakeAttendanceComponent } from './components/take-attendance/take-attendance.component';
+import { AllAttendanceComponent } from './components/all-attendance/all-attendance.component';
 
 const routes: Routes = [
   {
@@ -15,6 +23,30 @@ const routes: Routes = [
   },
   {
     path:'home',component:HomeComponent
+  },
+  {
+    path:'add-user',component:AddUserComponent
+  },
+  {
+    path:'all-user',component:AllUserComponent
+  },
+  {
+    path:'all-student',component:AllStudentComponent
+  },
+  {
+    path:'add-student',component:AddStudentComponent
+  },
+  {
+    path:'all-subject',component:AllSubjectComponent
+  },
+  {
+    path:'add-subject',component:AddSubjectComponent
+  },
+  {
+    path:'add-attendance',component:TakeAttendanceComponent
+  },
+  {
+    path:'all-attendance',component:AllAttendanceComponent
   }
 ];
 
